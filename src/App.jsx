1fc7671bdfe760074b0ca6504492eb585e64c31f
Vincent Kipngetich <vinniebluz@icloud.com>
2023-03-23 00:37:21 +0300

    Rect router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,72 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Gigs from "./pages/gigs/Gigs";
+import Gig from "./pages/gig/Gig";
+import Orders from "./pages/orders/Orders";
+import Mygigs from "./pages/myGigs/Mygigs";
+import Add from "./pages/add/Add";
+import Messages from "./pages/messages/Messages";
+import Message from "./pages/message/Message";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+import './App.scss'
 
 function App() {
+  const LayOut = () => {
+    return (
+      <>
+        <Navbar />
+        <Outlet />
+
+        <Footer />
+      </>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LayOut />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/gigs",
+          element: <Gigs />,
+        },
+        {
+          path: "/gig/:id",
+          element: <Gig />,
+        },
+        {
+          path: "/orders",
+          element: <Orders />,
+        },
+        {
+          path: "/mygigs",
+          element: <Mygigs />,
+        },
+        {
+          path: "/add",
+          element: <Add />,
+        },
+        {
+          path: "/messages",
+          element: <Messages />,
+        },
+        {
+          path: "/message/:id",
+          element: <Message />,
+        },
+      ],
+    },
+  ]);
   return (
     <div>
-      <Navbar />
+      <RouterProvider router={router} />
     </div>
   );
 }
