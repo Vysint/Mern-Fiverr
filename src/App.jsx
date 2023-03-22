d5d46ad722d5979fa956f0e2bbc9184d8898eca3
Vincent Kipngetich <vinniebluz@icloud.com>
2023-03-22 23:03:23 +0300

    Displated Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import Navbar from './components/navbar/Navbar';
 
 
 function App() {
   return (
     <div>
-      Hello world!
-      pleasure meeting you!
+      <Navbar />
     </div>
   )
 }
