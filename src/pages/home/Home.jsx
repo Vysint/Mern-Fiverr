11f30759d1af10cf016365315194172a59209ebb
Vincent Kipngetich <vinniebluz@icloud.com>
2023-03-27 21:52:01 +0300

    Added slider display

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import Featured from '../../components/featured/Featured';
-import TrustedBy from '../../components/trustedBy/TrustedBy';
+import React from "react";
+import CategoryCard from "../../components/CategoryCard/CategoryCard";
+import Featured from "../../components/featured/Featured";
+import Slide from "../../components/Slide/Slide";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
+import { cards } from "../../data";
 
-import './Home.scss'
+import "./Home.scss";
 
 const Home = () => {
   return (
     <div className="home">
       <Featured />
       <TrustedBy />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map((card) => (
+          <CategoryCard key={card.id} item={card} />
+        ))}
+      </Slide>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
