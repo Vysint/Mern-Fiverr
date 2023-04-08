c1db96e0584d493e649e7b4531111bc26c65b7c1
Vincent Kipngetich <vinniebluz@icloud.com>
2023-04-08 23:46:13 +0300

    added MyGigs section/pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./pages/home/Home";
 import Gigs from "./pages/gigs/Gigs";
 import Gig from "./pages/gig/Gig";
 import Orders from "./pages/orders/Orders";
-import Mygigs from "./pages/myGigs/Mygigs";
+import MyGigs from "./pages/myGigs/Mygigs";
 import Add from "./pages/add/Add";
 import Messages from "./pages/messages/Messages";
 import Message from "./pages/message/Message";
@@ -47,7 +47,7 @@ function App() {
         },
         {
           path: "/mygigs",
-          element: <Mygigs />,
+          element: <MyGigs />,
         },
         {
           path: "/add",
